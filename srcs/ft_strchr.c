df56f65d1cd0e7d6fca1ae27ba26fe78d5e1f40e
tviejo <tviejo@Thomas-V>
2024-03-26 17:03:43 +0100

    ft_strchr

---
+++ srcs/ft_strchr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tviejo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 16:37:09 by tviejo            #+#    #+#             */
+/*   Updated: 2024/03/26 17:03:17 by tviejo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)s + i);
+	else
+		return (NULL);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+int main(void)
+{
+        char    s1[] = "ABCDEGHIDEFJKLMNOP";
+        char    s2 = '\0';
+	char	*s3;
+
+	s3 = strchr(s1, s2);
+        printf("originale:%s", s3);
+	printf("  adresse:%p\n", s3);
+        s3 = ft_strchr(s1, s2);
+        printf("fonction:%s", s3);
+	printf("  adresse:%p\n", s3);
+}
+*/
