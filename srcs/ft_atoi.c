e3529205940b3d1632cc9846cbb8b3cb2fe3fe4f
tviejo <tviejo@Thomas-V>
2024-03-27 17:59:27 +0100

    atoi strnstr strdup

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,8 +6,56 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 18:07:47 by tviejo            #+#    #+#             */
-/*   Updated: 2024/03/26 18:07:52 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/03/27 17:39:16 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(const char *str)
+{
+	long int	cpt;
+	int			sign;
+	long int	value;
 
+	cpt = 0;
+	value = 0;
+	sign = 1;
+	while ((str[cpt] == ' ' || (9 <= str[cpt] && str[cpt] <= 13)))
+		cpt++;
+	if (str[cpt] == '-' || str[cpt] == '+')
+	{
+		if (str[cpt] == '-')
+			sign *= -1;
+		cpt++;
+	}
+	while ('0' <= str[cpt] && str[cpt] <= '9')
+	{
+		value = (value * 10) + (str[cpt] - '0');
+		cpt++;
+	}
+	return (value * sign);
+}
+/*
+#include <stdlib.h>
+#include <stdio.h>
+int	main(void)
+{
+	printf("originale: %d\n", atoi("            -2147483648"));
+	printf("fonction: %d\n", ft_atoi("            -2147483648"));
+	printf("\noriginale: %d\n", atoi("     +2147483647"));
+	printf("fonction: %d\n", ft_atoi("     +2147483647"));
+	printf("\noriginale:%d\n", atoi("-----+152 566"));
+	printf("fonction: %d\n", ft_atoi("-----+152 566"));
+	printf("\noriginale:%d\n", atoi("-----+152a566"));
+	printf("fonction: %d\n", ft_atoi("-----+152a566"));
+	printf("\noriginale:%d\n", atoi("            	\v 52	566"));
+	printf("fonction: %d\n", ft_atoi("              \v 52   566"));
+	printf("\noriginale:%d\n", atoi("52566"));
+	printf("fonction: %d\n", ft_atoi("52566"));
+	printf("\noriginale:%d\n", atoi("0"));
+	printf("fonction: %d\n", ft_atoi("0"));
+	printf("\noriginale:%d\n", atoi("-0"));
+	printf("fonction: %d\n", ft_atoi("-0"));
+	printf("\noriginale:%d\n", atoi(""));
+	printf("fonction: %d\n", ft_atoi(""));
+}
+*/
--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 18:00:47 by tviejo            #+#    #+#             */
-/*   Updated: 2024/03/26 18:44:13 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/03/27 17:25:53 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,31 @@
 
 char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
-        char    *str_temp;
-	size_t		i;
-        int             cs;
-        int             cf;
+	char	*str_temp;
+	size_t	cs;
+	size_t	cf;
 
-        cs = 0;
-        cf = 0;
-	i = 0;
-        if (to_find[0] == '\0')
-                return ((char *)str);
-        while (str[cs] != '\0' && i < len)
-        {
-                if ((to_find[cf] == '\0') || ((str[cs] == to_find[cf])
-                                && (cf > 0) && (to_find[cf + 1] == '\0')))
-                        return (str_temp);
-                else if ((str[cs] != to_find[cf]) && (cf > 0)
-				&& (to_find[cf] != '\0'))
-                        cf = 0;
-                else if ((str[cs] == to_find[cf]) && (cf > 0)
-				&& (to_find[cf] != '\0'))
-                        cf++;
-                if (str[cs] == to_find[cf] && cf == 0)
-                {
-                        str_temp = &(((char *)str)[cs]);
-                        cf++;
-                }
-		i++;
-                cs++;
-        }
-        return ("\0");
+	cs = 0;
+	cf = 0;
+	if (to_find[0] == '\0')
+		return ((char *)str);
+	while (str[cs] != '\0' && cs++ < len)
+	{
+		if ((to_find[cf] == '\0') || ((str[cs] == to_find[cf])
+				&& (cf > 0) && (to_find[cf + 1] == '\0')))
+			return (str_temp);
+		else if ((str[cs] != to_find[cf]) && (cf > 0)
+			&& (to_find[cf] != '\0'))
+			cf = 0;
+		else if (str[cs] == to_find[cf] && cf > 0
+			&& to_find[cf] != '\0')
+			cf++;
+		if (str[cs] == to_find[cf] && cf++ == 0)
+			str_temp = &(((char *)str)[cs]);
+	}
+	return ("\0");
 }
-
+/*
 #include <string.h>
 #include <stdio.h>
 int main(void)
@@ -54,6 +47,7 @@ int main(void)
         char    s2[] = "DEF";
 	char	*s3;
 
-        s3 = ft_strnstr(s1, s2, 5);
+        s3 = ft_strnstr(s1, s2, 15);
         printf("%s\n", s3);
 }
+*/
